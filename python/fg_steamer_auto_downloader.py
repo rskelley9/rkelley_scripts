d786b4a0330be34be4092b34050f7b1247c801d1
Ryan S Kelley <rskelley9@gmail.com>
2018-02-22 01:51:25 -0500

    Update fg_steamer_auto_downloader.py

--- python/fg_steamer_auto_downloader.py
+++ python/fg_steamer_auto_downloader.py
@@ -1,45 +1,45 @@
-import time;
-import os;
-import sys;
-from selenium import webdriver;
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile;
+import time
+import os
+import sys
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
 if len(sys.argv) > 1:
-	download_path = os.path.expanduser(sys.argv[1]);
+	download_path = os.path.expanduser(sys.argv[1])
 else:
-	download_path = os.path.expanduser('~/Desktop');
+	download_path = os.path.expanduser('~/Desktop')
 
 if not os.path.isdir(download_path):
-	sys.exit('Invalid path %s.'%(download_path));
+	sys.exit('Invalid path %s.'%(download_path))
 
-print('File will be saved to %s'%(download_path));
+print('File will be saved to %s'%(download_path))
 
-download_file_path = (download_path+'/Fangraphs Leaderboard.csv');
-ts_file_name = (time.strftime("%Y%m%d")+"_steamer_projections.csv"); ## custom file name
-profile = FirefoxProfile();
+download_file_path = (download_path+'/Fangraphs Leaderboard.csv')
+ts_file_name = (time.strftime("%Y%m%d")+"_steamer_projections.csv") ## custom file name
+profile = FirefoxProfile()
 
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk", 'text/csv');
-profile.set_preference("browser.download.manager.showWhenStarting", False);
-profile.set_preference("browser.download.dir", download_path);
-profile.set_preference("browser.download.folderList", 2); ## download to last location set
+profile.set_preference("browser.helperApps.neverAsk.saveToDisk", 'text/csv')
+profile.set_preference("browser.download.manager.showWhenStarting", False)
+profile.set_preference("browser.download.dir", download_path)
+profile.set_preference("browser.download.folderList", 2) ## download to last location set
 
-driver = webdriver.Firefox(firefox_profile=profile);
+driver = webdriver.Firefox(firefox_profile=profile)
 
-uri = "https://www.fangraphs.com/projections.aspx?pos=all&stats=bat&type=steamer&team=0&lg=all&players=0";
+uri = "https://www.fangraphs.com/projections.aspx?pos=all&stats=bat&type=steamer&team=0&lg=all&players=0"
 
-driver.get(uri);
-driver.find_element_by_link_text('Export Data').click();
+driver.get(uri)
+driver.find_element_by_link_text('Export Data').click()
 
 ## leave browser open for 5 seconds then close
-time.sleep(5);
-driver.quit();
-print('Success, file saved to %s'%(download_path));
+time.sleep(5)
+driver.quit()
+print('Success, file saved to %s'%(download_path))
 
 if os.path.isfile(download_file_path):
-	os.rename(download_file_path, ts_file_name );
-	print('Renamed file %s to %s'%(download_path,ts_file_name));
+	os.rename(download_file_path, ts_file_name )
+	print('Renamed file %s to %s'%(download_path,ts_file_name))
 else:
-	sys.exit('Error, unable to locate file at %s'%(download_path));
+	sys.exit('Error, unable to locate file at %s'%(download_path))
 
 
 
